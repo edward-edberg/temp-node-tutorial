c1b79f637941990dd7d5c4eced0c683d0d6f3862
Edward Edberg <edward.edberg.halim@gmail.com>
2022-03-22 02:22:28 -0700

    use logger

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,59 +1,25 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
+const logger = require("./logger");
 
+// req => middleware => res
+app.use("/api", logger);
 app.get("/", (req, res) => {
-  res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
-  // res.json(products);
+  res.send("Home");
+});
+
+app.get("/about", (req, res) => {
+  res.send("About");
 });
 
 app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProducts);
+  res.send("Products");
 });
 
-app.get("/api/products/:productID", (req, res) => {
-  // console.log(req);
-  // console.log(req.params);
-  const { productID } = req.params;
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-  if (!singleProduct) {
-    return res.status(404).send("Product does not exist");
-  }
-  res.json(singleProduct);
-});
-
-app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
-  console.log(req.params);
-  res.send("Hello World");
-});
-
-app.get("/api/v1/query", (req, res) => {
-  // console.log(req.query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (sortedProducts.length < 1) {
-    return res.status(200).json({ success: true, data: [] });
-  }
-  return res.status(200).json(sortedProducts);
-  res.send("hello world");
+app.get("/api/items", (req, res) => {
+  res.send("Items");
 });
 
 app.listen(5000, () => {
-  console.log("Server is listening on port 5000");
+  console.log("Server is listening on port 5000...");
 });
