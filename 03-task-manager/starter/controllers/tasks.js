e3aea3adea0fc03cdee407adfdf80ddad9397bfc
Edward Edberg <edward.edberg.halim@gmail.com>
2022-03-24 23:33:06 -0700

    add findONe and findAll

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,14 +1,33 @@
 const Task = require("../models/Task");
-const getAllTasks = (req, res) => {
-  res.send("all items from the file");
+const getAllTasks = async (req, res) => {
+  try {
+    const task = await Task.find({});
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: "error" });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: "error" });
+  }
 };
-const getTask = (req, res) => {
-  res.send("get single task");
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: "error" });
+  }
 };
 const updateTask = (req, res) => {
   res.send("update task");
