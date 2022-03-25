a5fb206ee3babd404c77966828477735750a335d
Edward Edberg <edward.edberg.halim@gmail.com>
2022-03-24 23:53:14 -0700

    add delete

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -32,8 +32,19 @@ const getTask = async (req, res) => {
 const updateTask = (req, res) => {
   res.send("update task");
 };
-const deleteTask = (req, res) => {
-  res.send("delete task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: "error" });
+  }
+  // res.send("delete task");
 };
 
 module.exports = {
