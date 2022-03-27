57b8888ba0985e6ac895a14f87d58df8d0187bc4
Edward Edberg <edward.edberg.halim@gmail.com>
2022-03-27 00:54:41 -0700

    pagination

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -3,7 +3,11 @@ const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
   // throw new Error("testing async error");
 
-  const products = await Product.find({}).select("name price");
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort("name")
+    .select("name price");
+  // .limit(10)
+  // .skip(1);
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -35,9 +39,15 @@ const getAllProducts = async (req, res) => {
     console.log(fieldsList);
   }
 
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
   const products = await Product.find(queryObject)
     .sort(`${sortList}`)
-    .select(`${fieldsList}`);
+    .select(`${fieldsList}`)
+    .skip(skip)
+    .limit(limit);
 
   res.status(200).json({ products, nbHits: products.length });
 };
