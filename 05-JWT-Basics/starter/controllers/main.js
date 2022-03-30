3bb6f99c9087c835e550e6bb5aa403bd698f5d25
Edward Edberg <edward.edberg.halim@gmail.com>
2022-03-30 01:37:14 -0700

    generate JWT

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,9 +1,30 @@
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+
 const login = async (req, res) => {
-  res.send("Fake Login/Register/Signup Route");
+  const { username, password } = req.body;
+
+  // mongoose validation
+  // Joi
+  // check in the controller
+
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide email and password", 400);
+  }
+
+  // just for demo
+  const id = new Date().getDate();
+  // keep payload small
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  // console.log(req.body);
+  // console.log(username, password);
+  res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Marh.floor(Math.random() * 100);
+  const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello, Edward Edberg`,
     secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
