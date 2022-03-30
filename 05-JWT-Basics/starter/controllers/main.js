a4a70fe302db16a7e3246ef6c7af83dd682455bb
Edward Edberg <edward.edberg.halim@gmail.com>
2022-03-30 11:12:51 -0700

    add errors middleware

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const CustomAPIError = require("../errors/custom-error");
+const { BadRequestError } = require("../errors");
 
 const login = async (req, res) => {
   const { username, password } = req.body;
@@ -9,7 +9,7 @@ const login = async (req, res) => {
   // check in the controller
 
   if (!username || !password) {
-    throw new CustomAPIError("Please provide email and password", 400);
+    throw new BadRequestError("Please provide email and password");
   }
 
   // just for demo
@@ -24,9 +24,10 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
+  console.log(req.user);
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
-    msg: `Hello, Edward Edberg`,
+    msg: `Hello, ${req.user.username}`,
     secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
   });
 };
