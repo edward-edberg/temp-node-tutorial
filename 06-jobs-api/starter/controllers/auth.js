5952cd98dcc5baef89a6a8c333c6dd96979437b2
Edward Edberg <edward.edberg.halim@gmail.com>
2022-03-31 01:36:55 -0700

    hash password

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,7 +1,8 @@
 const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
+const bcrypt = require("bcryptjs");
+
 const register = async (req, res) => {
-  // res.send("register user");
   const user = await User.create({ ...req.body });
   res.status(StatusCodes.CREATED).json({ user });
 };
