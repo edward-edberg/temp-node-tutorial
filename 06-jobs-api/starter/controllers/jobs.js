f99225a0893de7c812e75b7a83e0ec702f6f0e5e
Edward Edberg <edward.edberg.halim@gmail.com>
2022-04-04 15:22:18 -0700

    add auto auth

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -1,11 +1,20 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
 const getAllJobs = async (req, res) => {
-  res.send("get all jobs");
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
+  // res.send("get all jobs");
 };
 const getJob = async (req, res) => {
   res.send("get job");
 };
 const createJob = async (req, res) => {
-  res.json(req.user);
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
+  // res.json(req.body);
 };
 const updateJob = async (req, res) => {
   res.send("update job");
